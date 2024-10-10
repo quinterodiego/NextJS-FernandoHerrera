268ee357394f6f566c15f799310219ede65b8945
quinterodiego <d86webs@gmail.com>
2024-10-09 23:50:33 -0300

    asd

---
+++ 99-typescript/src/App.tsx
@@ -0,0 +1,12 @@
+import { Counter } from "./components/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola Mundo con Typescript</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
